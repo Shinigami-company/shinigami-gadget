26bb9cde0ea9660fbafe658e6cf1eeca4fc25135
aktomik <97125853+AKtomik@users.noreply.github.com>
2025-04-21 03:30:31 +0200

    adding models too

---
+++ api/models/KiraItems/schema.gadget.ts
@@ -0,0 +1,18 @@
+import type { GadgetModel } from "gadget-server";
+
+// This file describes the schema for the "KiraItems" model, go to https://shinigami.gadget.app/edit to view/edit your model in Gadget
+// For more information on how to update this file http://docs.gadget.dev
+
+export const schema: GadgetModel = {
+  type: "gadget/model-schema/v1",
+  storageKey: "ajFsJt-RW_nN",
+  fields: {
+    itemId: { type: "string", storageKey: "J3mHD1CwWgaL" },
+    itemLoreTxt: { type: "richText", storageKey: "BR9ljaH87ShH" },
+    ownerPtr: {
+      type: "belongsTo",
+      parent: { model: "KiraUsers" },
+      storageKey: "FaNqfJXG3B4a",
+    },
+  },
+};
